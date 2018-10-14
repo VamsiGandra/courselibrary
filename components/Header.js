2accf812f1438d7aafb87a1dc5d5014eb3f0ea1b
NaveenDK <naveenkolambage@gmail.com>
2018-10-14 23:27:27 +1300

    added the menu - header - layout with semantic UI

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,28 @@
+import Link from 'next/link';
+
 const Header = () => (
+  
     <div>
-      <h1>This is header page</h1>
+    
+    <div className="ui menu">
+    <img className="logo"src='../static/logo.png'/>
+ 
+  <a className="active item">Home</a>
+  <a className="item">Become an Instructor</a>
+  
+  <div className="right menu">
+    <div className="item">
+      <div className="ui action left icon input">
+        <i className="search icon"></i>
+        <input type="text" placeholder="Search For Courses"/>
+        <button className="ui button">Submit</button>
+      </div>
     </div>
+    <a className="item">Sign In</a>
+  </div>
+</div>
+</div>
+   
   )
   
 export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import Header from '../components/Header';
+
+import Layout from '../components/Layout';
 
 const Index = () => (
-    <div>
-        <Header/>
-        <p>Hello Next.js</p>
+    <Layout>
+    <div className="ui container">
+       <br></br>
+        <p>TODO : Plan the next elements of the page - and how the authentication can be done using firebase</p>
     </div>
+    </Layout>
   )
   
-export default Index
+export default Index;
